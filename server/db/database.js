973bd7c2557fd33bb525c16a793bfb2db35c404d
Scott Rindahl <sdrindahl@gmail.com>
2025-12-08 15:34:04 -0600

    Move SQLite db to server/ and update db path

--- server/db/database.js
+++ server/db/database.js
@@ -6,7 +6,8 @@ import { promisify } from 'util';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dbPath = process.env.DB_PATH || path.join(__dirname, 'eyebridge.db');
+//const dbPath = process.env.DB_PATH || path.join(__dirname, 'eyebridge.db');
+const dbPath = process.env.DB_PATH || path.join(__dirname, '..', 'eyebridge.db');
 
 // Create database connection
 const db = new sqlite3.Database(dbPath);
